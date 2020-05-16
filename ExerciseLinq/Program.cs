277254a117008cc38c86d122a8287bad62882403
Thiago Silva <thiago.silva@outlook.com>
2020-05-16 14:03:39 -0300

    Exercicio LINQ 2

--- ExerciseLinq/Program.cs
+++ ExerciseLinq/Program.cs
@@ -14,7 +14,7 @@ namespace ExerciseLinq
             Console.Write("Enter full file path: ");
             string path = Console.ReadLine();
 
-            List<Product> products = new List<Product>();
+            List<Employee> employees = new List<Employee>();
 
             using (StreamReader sr = File.OpenText(path))
             {
@@ -22,18 +22,25 @@ namespace ExerciseLinq
                 {
                     string[] fields = sr.ReadLine().Split(',');
                     string name = fields[0];
-                    double price = double.Parse(fields[1],CultureInfo.InvariantCulture);
-                    products.Add(new Product(name, price));
+                    string email = fields[1];
+                    double salary = double.Parse(fields[2],CultureInfo.InvariantCulture);
+                    employees.Add(new Employee(name, email, salary));
                 }
             }
 
-            var avg = products.Select(p => p.Price).DefaultIfEmpty(0.0).Average();
-            Console.WriteLine("Average price: " + avg.ToString("F2",CultureInfo.InvariantCulture));
-            var names = products.Where(p => p.Price < avg).OrderByDescending(p => p.Name).Select(p => p.Name);
-            foreach(string p in names)
+            Console.Write("Enter salary: ");
+            double sal = double.Parse(Console.ReadLine());
+            Console.WriteLine("Email of people whose salary is more than " + sal.ToString("F2", CultureInfo.InvariantCulture) + ":");
+
+            var emails = employees.Where(e => e.Salary > sal).Select(e => e.Email);
+            foreach(string email in emails)
             {
-                Console.WriteLine(p);
+                Console.WriteLine(email);
             }
+            var sumM = employees.Where(e => e.Name[0] == 'M').Sum(e => e.Salary);
+            Console.WriteLine();
+            Console.WriteLine("Sum of salary of people whose name starts with 'M': " + sumM.ToString("F2", CultureInfo.InvariantCulture));
+            
         }
     }
 }
